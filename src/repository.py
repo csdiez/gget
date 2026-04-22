c25b4c68d6e4346f41853c1c5b948ed0189ea758
Christian Diez <christian.diez1004@gmail.com>
2026-04-22 10:33:44 -0700

    Repository commands in progress

--- src/repository.py
+++ src/repository.py
@@ -1,1 +1,11 @@
 import subprocess
+
+def clone(url:str, *args, **kwargs) -> None:
+    call = ['git', 'clone', url, 'save']
+    for k, v in kwargs.items():
+        call.append(k)
+        call.append(v)
+        
+    subprocess.call(call)
+    
+clone("https://github.com/csdiez/gget.git")
