dc52ceafdb6374038f662080caa5951b2558d510
Christian Diez <christian.diez1004@gmail.com>
2026-05-09 22:19:39 -0700

    Improving save functionality for consistency

--- src/gui.py
+++ src/gui.py
@@ -2,28 +2,29 @@ import logging
 
 import PySimpleGUI as sg
 
-from config import load_config, save_config
 import directory as d
+from config import load_config, save_config
 from main import cmd_pull, cmd_push
 from repository import ping, remote_saves
+
 
 def confirmation(prompt: str) -> bool:
     decision = False
 
     layout = [
         [sg.Push(), sg.Text(prompt), sg.Push()],
-        [sg.Push(), sg.Button('Confirm'), sg.Button('Cancel'), sg.Push()]
+        [sg.Push(), sg.Button("Confirm"), sg.Button("Cancel"), sg.Push()],
     ]
 
-    window = sg.Window('', layout)
+    window = sg.Window("", layout)
 
     while 1:
         event, _ = window.read()
 
-        if event in [sg.WIN_CLOSED, 'Cancel']:
+        if event in [sg.WIN_CLOSED, "Cancel"]:
             break
 
-        if event == 'Confirm':
+        if event == "Confirm":
             decision = True
             break
 
@@ -31,10 +32,11 @@ def confirmation(prompt: str) -> bool:
 
     return decision
 
+
 def text_and_close(title: str, text: str) -> None:
     layout = [
         [sg.Push(), sg.Text(text), sg.Push()],
-        [sg.Push(), sg.Button('Close', focus=True), sg.Push()]
+        [sg.Push(), sg.Button("Close", focus=True), sg.Push()],
     ]
     print(text)
     window = sg.Window(title, layout)
@@ -47,23 +49,27 @@ def text_and_close(title: str, text: str) -> None:
 
     window.close()
 
+
 def info(text: str) -> None:
     logging.info(text)
-    text_and_close('Info', text)
+    text_and_close("Info", text)
+
 
 def warning(text: str) -> None:
     logging.warning(text)
-    text_and_close('Warning', text)
+    text_and_close("Warning", text)
+
 
 def error(text: str) -> None:
     logging.info(text)
-    text_and_close('Error', text)
+    text_and_close("Error", text)
+
 
 def add_game() -> tuple[str, str] | None:
     layout = [
         [sg.Text("Game Title"), sg.Push(), sg.InputText()],
         [sg.Text("Game Directory"), sg.Push(), sg.InputText()],
-        [sg.Button('Enter'), sg.Button('Cancel')]
+        [sg.Button("Enter"), sg.Button("Cancel")],
     ]
 
     window = sg.Window("Add Game", layout)
@@ -73,10 +79,10 @@ def add_game() -> tuple[str, str] | None:
     while 1:
         event, values = window.read()
 
-        if event in [sg.WIN_CLOSED, 'Cancel']:
+        if event in [sg.WIN_CLOSED, "Cancel"]:
             break
 
-        if event == 'Enter':
+        if event == "Enter":
             game_title = values[0]
             dir = values[1]
 
@@ -94,24 +100,25 @@ def add_game() -> tuple[str, str] | None:
 
     return game
 
+
 def remove_game() -> str:
     config = load_config()
-    games = {game.replace('_', ' '): game for game in config.keys()}
+    games = {game.replace("_", " "): game for game in config.keys()}
 
     layout: list = [
         [sg.Text("Choose a game to remove:")],
         [[sg.Button(game)] for game in games.keys()],
-        [sg.Push(), sg.Button("Cancel"), sg.Push()]
+        [sg.Push(), sg.Button("Cancel"), sg.Push()],
     ]
 
     window = sg.Window("Remove Game", layout)
 
-    game: str = ''
+    game: str = ""
 
     while 1:
         event, _ = window.read()
 
-        if event in (sg.WIN_CLOSED, 'Cancel'):
+        if event in (sg.WIN_CLOSED, "Cancel"):
             break
 
         if event:
@@ -124,6 +131,7 @@ def remove_game() -> str:
 
     return game
 
+
 def load_game(game_name: str) -> None:
     try:
         cmd_pull(game_name)
@@ -132,34 +140,56 @@ def load_game(game_name: str) -> None:
         err_code = se.args[1]
         stderr = se.args[2]
 
-        warning(f"Couldn't load saves for {game_name}\nCommand: {git_cmd}\nError Code {err_code}: {stderr}")
+        warning(
+            f"Couldn't load saves for {game_name}\nCommand: {git_cmd}\nError Code {err_code}: {stderr}"
+        )
+
 
 def main():
     config = load_config()
 
     restart = False
 
-    online = not bool(ping())
-    
+    online = True or not bool(ping(15))
+
     saved = remote_saves()
 
     dir_rows = [
         [
-            sg.Text(game.replace('_', ' ') + ':'), sg.Push(), sg.InputText(dir),
-            sg.Button('↧', tooltip="Download" if game in saved else "No available save", disabled=game not in saved), sg.Button('↥', tooltip="Upload")
-        ] for game, dir in config.items()
+            sg.Text(game.replace("_", " ") + ":"),
+            sg.Push(),
+            sg.InputText(dir),
+            sg.Button(
+                "↧",
+                tooltip="Download" if game in saved else "No available save",
+                disabled=game not in saved,
+            ),
+            sg.Button("↥", tooltip="Upload"),
+        ]
+        for game, dir in config.items()
     ]
 
     dir_keys = list(config.keys())
-    
+
     layout = [
         [
-            sg.Button('+', tooltip="Add new directory"), sg.Button('-', tooltip="Remove a directory"), sg.Push(),
-            sg.Text('🟢' if online else '🔴', tooltip='Online' if online else 'Offline'),
-            sg.Button('↧', tooltip="Download"), sg.Button('↥', tooltip="Upload")
+            sg.Button("+", tooltip="Add new directory"),
+            sg.Button("-", tooltip="Remove a directory"),
+            sg.Push(),
+            sg.Text(
+                "🟢" if online else "🔴", tooltip="Online" if online else "Offline"
+            ),
+            sg.Button("↧", tooltip="Download"),
+            sg.Button("↥", tooltip="Upload"),
         ],
         dir_rows,
-        [sg.Push(), sg.Button("Save", tooltip="Save current directories"), sg.Button("Refresh", tooltip="Reopen this window"), sg.Button('Close', tooltip="Close Window"), sg.Push()]
+        [
+            sg.Push(),
+            sg.Button("Save", tooltip="Save current directories"),
+            sg.Button("Refresh", tooltip="Reopen this window"),
+            sg.Button("Close", tooltip="Close Window"),
+            sg.Push(),
+        ],
     ]
 
     window = sg.Window("Gget", layout)
@@ -167,19 +197,19 @@ def main():
     while 1:
         event, values = window.read()
 
-        if event in [sg.WIN_CLOSED, 'Close']:
+        if event in [sg.WIN_CLOSED, "Close"]:
             break
 
-        if event == 'Refresh':
+        if event == "Refresh":
             restart = True
             break
 
-        if event == 'Save' and isinstance(values, dict):
+        if event == "Save" and isinstance(values, dict):
             for i, dir in values.items():
                 config[dir_keys[i]] = dir
             save_config(config)
 
-        if event == '+':
+        if event == "+":
             game = add_game()
             if not game:
                 warning("Empty input")
@@ -190,11 +220,11 @@ def main():
             if game_name in config:
                 warning(f"{game_name} already exists")
                 continue
-            
+
             if not d.exists(dir):
                 warning(f"Directory does not exist\n{dir}")
                 continue
-            
+
             config[game_name] = dir
             save_config(config)
 
@@ -203,7 +233,7 @@ def main():
             restart = True
             break
 
-        if event == '-':
+        if event == "-":
             game = remove_game()
 
             info(f"Removed {game}")
@@ -211,17 +241,17 @@ def main():
             restart = True
             break
 
-        if event == '↧':
-            if not confirmation('This will overwrite your local save data, continue?'):
+        if event == "↧":
+            if not confirmation("This will overwrite your local save data, continue?"):
                 continue
-            
+
             for game in config.keys():
                 load_game(game)
 
             restart = True
             break
 
-        elif event == '↥':
+        elif event == "↥":
             diffs: dict[str, bool] = {}
             for game in config.keys():
                 diffs[game] = cmd_push(game)
@@ -235,12 +265,14 @@ def main():
                 info("No changes to upload.")
 
         elif isinstance(event, str):
-            if '↧' in event:
+            if "↧" in event:
                 game = dir_keys[int(event[1]) // 2]
-                
-                if not confirmation(f'This will overwrite your local save data, continue?\n{game}'):
+
+                if not confirmation(
+                    f"This will overwrite your local save data, continue?\n{game}"
+                ):
                     continue
-                
+
                 load_game(game)
 
                 info(f"Loaded saves for {game}.")
@@ -248,7 +280,7 @@ def main():
                 restart = True
                 break
 
-            elif '↥' in event:
+            elif "↥" in event:
                 game = dir_keys[(int(event[1]) - 1) // 2]
                 changed = cmd_push(game)
 
@@ -259,7 +291,7 @@ def main():
                     break
 
                 else:
-                    info(f"No changes to upload.")
+                    info("No changes to upload.")
 
         # if event:
         #     print(f"{event=}")
@@ -268,11 +300,12 @@ def main():
         #     print(f"{values=}")
 
         pass
-    
+
     window.close()
 
     if restart:
         main()
+
 
 if __name__ == "__main__":
     main()
--- src/repository.py
+++ src/repository.py
@@ -1,23 +1,27 @@
 import logging
+import subprocess
 from multiprocessing import Manager, Process
 from pathlib import Path
-
-import subprocess
 from typing import Any
 
 from directory import BARE_REPO
 
-def git(*args: Path | str, timeout: int = 30, **kwargs: Any) -> subprocess.CompletedProcess:
-    cmd_args = ['git']
+
+def git(
+    *args: Path | str, timeout: int = 30, **kwargs: Any
+) -> subprocess.CompletedProcess:
+    cmd_args = ["git"]
     cmd_args.extend([str(arg) for arg in args])
-    
-    cmd_str = ' '.join(cmd_args)
+
+    cmd_str = " ".join(cmd_args)
 
     m = Manager()
     response = m.dict()
 
     def check(d: dict[str, subprocess.CompletedProcess]) -> None:
-        r = subprocess.run(cmd_args, **(kwargs or {"capture_output": True, "text": True}))
+        r = subprocess.run(
+            cmd_args, **(kwargs or {"capture_output": True, "text": True})
+        )
         d[cmd_str] = r
 
     p = Process(target=check, args=[response])
@@ -30,41 +34,66 @@ def git(*args: Path | str, timeout: int = 30, **kwargs: Any) -> subprocess.Compl
         if result.returncode:
             logging.error(cmd_str, result.returncode, result.stderr)
             raise SystemError(cmd_str, result.returncode, result.stderr)
-    except AssertionError as ae:
+    except subprocess.CalledProcessError as e:
         p.terminate()
         p.join()
-        raise ae
+        raise e
     else:
         logging.info(cmd_str, result.stdout)
         return result
+
 
 def git_output(*args: str, timeout: int = 30, **kwargs: Any) -> str:
     """Same as git() but returns stdout as a stripped string."""
     return git(*args, timeout=timeout, **kwargs).stdout.strip()
 
-def git_games(*args: str, timeout: int = 30, work_tree: Path | None = None, **kwargs: Any) -> subprocess.CompletedProcess:
+
+def git_games(
+    *args: str, timeout: int = 30, work_tree: Path | None = None, **kwargs: Any
+) -> subprocess.CompletedProcess:
     if work_tree:
-        return git(f'--work-tree={work_tree}', f"--git-dir={BARE_REPO}", *args, timeout=timeout, **kwargs)
+        return git(
+            f"--work-tree={work_tree}",
+            f"--git-dir={BARE_REPO}",
+            *args,
+            timeout=timeout,
+            **kwargs,
+        )
     else:
         return git(f"--git-dir={BARE_REPO}", *args, timeout=timeout, **kwargs)
 
-def git_games_output(*args, timeout: int = 30, work_tree: Path | None = None, **kwargs: Any) -> str:
+
+def git_games_output(
+    *args, timeout: int = 30, work_tree: Path | None = None, **kwargs: Any
+) -> str:
     if work_tree:
-        return git_output(f'--work-tree={work_tree}', f"--git-dir={BARE_REPO}", *args, timeout=timeout, **kwargs)
+        return git_output(
+            f"--work-tree={work_tree}",
+            f"--git-dir={BARE_REPO}",
+            *args,
+            timeout=timeout,
+            **kwargs,
+        )
     else:
         return git_output(f"--git-dir={BARE_REPO}", *args, timeout=timeout, **kwargs)
 
-def ping() -> int:
+
+def ping(timeout: int = 30) -> int:
     """
     Ping loaded repo from the home directory
     Sends the return code from the command (0=pass, else=fail)
     """
-    return git_games('ls-remote').returncode
+    try:
+        return git_games("ls-remote", timeout=timeout).returncode
+    except subprocess.CalledProcessError as e:
+        print(e, f"Timed out after {timeout} seconds")
+        return 1
+
 
 def remote_saves() -> list[str]:
     """Return all top-level directories in the remote repo."""
-    result = git_games("ls-tree", "-d", "--name-only", "origin/main",
-        capture_output=True, text=True
+    result = git_games(
+        "ls-tree", "-d", "--name-only", "origin/main", capture_output=True, text=True
     )
     if result.returncode != 0 or not result.stdout.strip():
         return []
